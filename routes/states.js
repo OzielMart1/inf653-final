2d05f8890f401733d05d877e5bd75c0611ed0478
Oziel Martinez <o_martinezmondragon@mail.fhsu.edu>
2025-05-07 17:55:43 -0500

    fixing routes

--- routes/states.js
+++ routes/states.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const statesController = require('../controllers/statesController');
 const verifyState = require('../middleware/verifyState');
 
+router.route('/')
+  .get(statesController.getAllStates);
+
 router.route('/:state')
   .get(verifyState, statesController.getState);
 
